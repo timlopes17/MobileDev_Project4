e0392368eb117ac52f0da11ac7bb2fc48d09bebf
timlopes17 <timlopes17@gmail.com>
2022-02-22 12:41:36 -0500

    Start of project

---
+++ app/src/main/java/edu/temple/timlopes_project4/ImageActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.timlopes_project4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
